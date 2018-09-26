38091885e8534016704cc4021b042e7fed903827
Jay Courtney <jmc5113@live.com>
2018-09-25 23:22:29 -0700

    updated to show entities intents and scores

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using System.Text;
 
 namespace Microsoft.Bot.Sample.LuisBot
 {
@@ -45,9 +46,36 @@ namespace Microsoft.Bot.Sample.LuisBot
             await this.ShowLuisResult(context, result);
         }
 
+        // Entities found in result
+        private string BotEntityRecognition(LuisResult result)
+        {
+            StringBuilder entityResults = new StringBuilder();
+
+            if (result.Entities.Count > 0)
+            {
+                foreach (EntityRecommendation item in result.Entities)
+                {
+                    // Query: Turn on the [light]
+                    // item.Type = "HomeAutomation.Device"
+                    // item.Entity = "light"
+                    entityResults.Append(item.Type + "=" + item.Entity + ",");
+                }
+                // remove last comma
+                entityResults.Remove(entityResults.Length - 1, 1);
+            }
+
+            return entityResults.ToString();
+        }
+
         private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
         {
-            await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
+            // get recognized entities
+            string entities = this.BotEntityRecognition(result);
+
+            // round number
+            string roundedScore = result.Intents[0].Score != null ? (Math.Round(result.Intents[0].Score.Value, 2).ToString()) : "0";
+
+            await context.PostAsync($"**Query**: {result.Query}, **Intent**: {result.Intents[0].Intent}, **Score**: {roundedScore}. **Entities**: {entities}");
             context.Wait(MessageReceived);
         }
     }
